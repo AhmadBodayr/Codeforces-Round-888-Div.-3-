90c9e87d4d98bd3dd4d626027f7409274b69e976
Ahmad Bodayr <ahmadbodayr@Ahmads-MacBook-Pro.local>
2023-07-24 21:18:51 +0300

    setup finished

---
+++ testing.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+typedef long long ll; 					
+typedef unsigned long long ull; 
+typedef long double ld;
+#define inf 1000000000000000005
+///////////////////////////////
+ll t, n;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL); 	
+
+	#ifndef ONLINE_JUDGDE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);	
+	#endif
+	///////////////////////////////////////////////////////////////
+	cin >> t;
+	while(t--) {
+		
+		
+		
+	} // end test cases
+} // end main
